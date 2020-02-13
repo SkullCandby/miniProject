5023c14811d7b7594dd2b754316678499fa05bd6
SkullCandby <42152517+SkullCandby@users.noreply.github.com>
2020-02-13 19:52:56 +0300

    done 1 ex

--- MainApp.py
+++ MainApp.py
@@ -3,7 +3,7 @@ import sys
 
 import requests
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QMainWindow
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QMainWindow, QLineEdit
 from map_design import Ui_MapsApp
 
 
@@ -13,12 +13,14 @@ class Map(QMainWindow, Ui_MapsApp):
         self.setupUi(self)
         self.pushButton.clicked.connect(self.do_request)
     def do_request(self):
-        self.width_txt = self.width.text()
-        self.lenght_txt = self.lenght.text()
-        self.width_size_txt = self.width_size.text()
-        self.lenght_size_txt = self.lenght_size.text()
-        print(self.width_txt)
-        map_request = f"http://static-maps.yandex.ru/1.x/?ll={self.width_txt},{self.lenght_txt}&spn={self.lenght_size_txt},{self.width_size_txt}&l=map"
+        width_txt = self.width.text()
+        print(width_txt)
+        length_txt = self.length.text()
+        print(length_txt)
+        width_size_txt = self.width_size.text()
+        length_size_txt = self.lenght_size.text()
+        print(width_txt, length_txt, width_size_txt, length_size_txt)
+        map_request = f"http://static-maps.yandex.ru/1.x/?ll={width_txt},{length_txt}&spn={length_size_txt},{width_size_txt}&l=map"
         response = requests.get(map_request)
         if not response:
             print("Ошибка выполнения запроса:")
@@ -32,7 +34,6 @@ class Map(QMainWindow, Ui_MapsApp):
         self.img_lbl.setPixmap(pixmap)
     def closeEvent(self, event):
         os.remove(self.map_file)
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
