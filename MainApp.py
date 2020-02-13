a24d9b33b02a958703ad983c6056cf5b212a40d0
SkullCandby <42152517+SkullCandby@users.noreply.github.com>
2020-02-13 23:02:38 +0300

    done 1 ex

--- MainApp.py
+++ MainApp.py
@@ -5,6 +5,7 @@ import requests
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QMainWindow, QLineEdit
 from map_design import Ui_MapsApp
+from PyQt5.QtCore import Qt
 
 
 class Map(QMainWindow, Ui_MapsApp):
@@ -12,15 +13,13 @@ class Map(QMainWindow, Ui_MapsApp):
         super().__init__()
         self.setupUi(self)
         self.pushButton.clicked.connect(self.do_request)
+
     def do_request(self):
         width_txt = self.width.text()
-        print(width_txt)
         length_txt = self.length.text()
-        print(length_txt)
-        width_size_txt = self.width_size.text()
-        length_size_txt = self.lenght_size.text()
-        print(width_txt, length_txt, width_size_txt, length_size_txt)
-        map_request = f"http://static-maps.yandex.ru/1.x/?ll={width_txt},{length_txt}&spn={length_size_txt},{width_size_txt}&l=map"
+        self.width_size_txt = self.width_size.text()
+        self.length_size_txt = self.lenght_size.text()
+        map_request = f"http://static-maps.yandex.ru/1.x/?ll={width_txt},{length_txt}&spn={self.length_size_txt},{self.width_size_txt}&l=map"
         response = requests.get(map_request)
         if not response:
             print("Ошибка выполнения запроса:")
@@ -32,8 +31,34 @@ class Map(QMainWindow, Ui_MapsApp):
             file.write(response.content)
         pixmap = QPixmap(self.map_file)
         self.img_lbl.setPixmap(pixmap)
+
     def closeEvent(self, event):
         os.remove(self.map_file)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp:
+            sw = str(int(float(self.width_size_txt)) + 1)
+            sl = str(int(float(self.length_size_txt)) + 1)
+            print(sw)
+            self.width_size.setText(sw)
+            self.lenght_size.setText(sl)
+            self.do_request()
+        if event.key() == Qt.Key_PageDown:
+            print((self.width_size_txt))
+            if (int(float(self.width_size_txt)) - 1) <= 0:
+                print(1)
+                sw = '0.001'
+            else:
+                sw = str(int(self.width_size_txt) - 1)
+            if int(float(self.width_size_txt)) - 1 <= 0:
+                sl = '0.001'
+            else:
+                sl = str(int(self.length_size_txt) - 1)
+            print(sw)
+            self.width_size.setText(sw)
+            self.lenght_size.setText(sl)
+            self.do_request()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
