75c311bd771cc899393b1a9faf6d165e714cd1ea
SkullCandby <gdmeshkov@mail.ru>
2020-02-13 18:26:43 +0300

    ui not dot py file

--- MainApp.py
+++ MainApp.py
@@ -3,50 +3,39 @@ import sys
 
 import requests
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QMainWindow
+from map_design import Ui_MapsApp
 
-SCREEN_SIZE = [600, 450]
 
-
-class Example(QWidget):
+class Map(QMainWindow, Ui_MapsApp):
     def __init__(self):
         super().__init__()
-        self.getImage()
-        self.initUI()
-
-    def getImage(self):
-        map_request = "http://static-maps.yandex.ru/1.x/?ll=37.530887,55.703118&spn=0.002,0.002&l=map"
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.do_request)
+    def do_request(self):
+        self.width_txt = self.width.text()
+        self.lenght_txt = self.lenght.text()
+        self.width_size_txt = self.width_size.text()
+        self.lenght_size_txt = self.lenght_size.text()
+        print(self.width_txt)
+        map_request = f"http://static-maps.yandex.ru/1.x/?ll={self.width_txt},{self.lenght_txt}&spn={self.lenght_size_txt},{self.width_size_txt}&l=map"
         response = requests.get(map_request)
-
         if not response:
             print("Ошибка выполнения запроса:")
             print(map_request)
             print("Http статус:", response.status_code, "(", response.reason, ")")
             sys.exit(1)
-
-        # Запишем полученное изображение в файл.
         self.map_file = "map.png"
         with open(self.map_file, "wb") as file:
             file.write(response.content)
-
-    def initUI(self):
-        self.setGeometry(100, 100, *SCREEN_SIZE)
-        self.setWindowTitle('Отображение карты')
-
-        ## Изображение
-        self.pixmap = QPixmap(self.map_file)
-        self.image = QLabel(self)
-        self.image.move(0, 0)
-        self.image.resize(600, 450)
-        self.image.setPixmap(self.pixmap)
-
+        pixmap = QPixmap(self.map_file)
+        self.img_lbl.setPixmap(pixmap)
     def closeEvent(self, event):
-        """При закрытии формы подчищаем за собой"""
         os.remove(self.map_file)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Example()
+    ex = Map()
     ex.show()
     sys.exit(app.exec())
